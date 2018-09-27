6ae21d503794b2233c01330c2702047f230de195
Nadben <33803808+Nadben@users.noreply.github.com>
2018-09-27 09:37:18 -0400

    Change - modbus addresses

--- p1kModbus.py
+++ p1kModbus.py
@@ -5,10 +5,12 @@ import minimalmodbus, time, csv, os, threading, shutil, sys
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import matplotlib.ticker as ticker
+
 ##import matplotlib.dates as mdates
 from matplotlib import style
 from matplotlib import pylab
-from threading import Thread
+from threading import Thread #not used
+
 
 
 """
@@ -18,7 +20,7 @@ Code fait par Nadir Benabdesselam
 Description:
 
     This script was written for OCO Technologies. It's a simple
-    datalogger for remote use. 
+    datalogger for remote use.
 
 
 """
@@ -53,7 +55,7 @@ def plottingDailyReport(graphName,infich_2):
     fig1 = plt.figure(1)
     fig1.suptitle('Co2',fontsize=20)
     fig_CO2 = fig1.add_subplot(111)
-    
+
     fig2 = plt.figure(2)
     fig2.suptitle('pH',fontsize=20)
     fig_pH = fig2.add_subplot(111)
@@ -101,16 +103,16 @@ def plottingDailyReport(graphName,infich_2):
     fig_qAir.yaxis.set_major_locator(ticker.MaxNLocator(5))
     fig_qAcid.yaxis.set_major_locator(ticker.MaxNLocator(5))
     fig_pressure.yaxis.set_major_locator(ticker.MaxNLocator(5))
-    
+
     co2_to_plot, qEau_to_plot, pH_to_plot, pressure_to_plot, qAcid_to_plot, qAir_to_plot, cntx = [],[],[],[],[],[],[]
-    data_set = open(infich_2,'r').read() 
-    lines = data_set.split('\n')    
+    data_set = open(infich_2,'r').read()
+    lines = data_set.split('\n')
     i = 0
-    
-    
+
+
     for line in lines :
         i+=1
-        
+
         if i in range(2,len(lines)):#skip header
             cntx.append(i)
             a,b,c,d,e,f,g,h,k,l,m,n,o,p,q = line.split(',')
@@ -120,43 +122,43 @@ def plottingDailyReport(graphName,infich_2):
             pressure_to_plot.append(int(k))
             qAcid_to_plot.append(int(f))
             qAir_to_plot.append(int(g))
-            
+
     #Plot processing
     fig_CO2.plot(cntx,co2_to_plot,linewidth=2)
     fig1.savefig(dst_co2+graphName)
     plt.close(fig1)
 
-    
+
     fig_pH.plot(cntx,pH_to_plot)
     fig2.savefig(dst_ph+graphName)
     plt.close(fig2)
 
-    
+
     fig_qAir.plot(cntx,qAir_to_plot,linewidth=2)
     fig3.savefig(dst_qair+graphName)
     plt.close(fig3)
 
-    
+
     fig_qEau.plot(cntx,qEau_to_plot,linewidth=2)
     fig4.savefig(dst_qeau+graphName)
     plt.close(fig4)
 
-    
+
     fig_pressure.plot(cntx,pressure_to_plot,linewidth=2)
     fig5.savefig(dst_pression+graphName)
     plt.close(fig5)
 
-    
+
     fig_qAcid.plot(cntx,qAcid_to_plot,linewidth=2)
     fig6.savefig(dst_qacide+graphName)
     plt.close(fig6)
 
 
-    
+
 
 
 def fileCopy(src,dest):
-    
+
     dest_dir = os.path.dirname(dest)
 
     try :
@@ -170,7 +172,7 @@ def fileCopy(src,dest):
 
 def shiftFile(line_count):
 
-    
+
     #rewrite tmpfile to newfile then rename newfile
     with open(tempFile,'r') as tmpfile :
         with open('newfile.csv', 'a') as newfile :
@@ -186,7 +188,7 @@ def shiftFile(line_count):
                             [int(sp[0]), int(sp[1]), int(sp[2]),
                              int(sp[3]), int(sp[4]), int(sp[5])]
                             )
-                        
+
     os.remove(tempFile)#remove temporary file
     os.rename('newfile.csv','tmp.csv') #rename newfile to oldfile
 
@@ -198,31 +200,36 @@ def checkFile(tmpfile):
 
     if line_count == 180 :
         shiftFile(line_count)
-        
 
 
-    
 
-#Lecture des sensors avec minimalmodbus 
+
+
+#Lecture des sensors avec minimalmodbus
 def sensorReadings(p2k,cntx):
 ##            DI = p2k.read_bit(0)
     #faire un try except block OSError
     try :
-        Co2 = p2k.read_register(0,functioncode=3) 
+        Co2 = p2k.read_register(0,functioncode=3)
     except OSError as ose :
         print("couldn't communicate with the plc! Verify the connection please. ")
         pass
-    
+
     else :
         DI = 0
         Co2 = p2k.read_register(0,functioncode=3)
+        qAir = p2k.read_register(1,functioncode=3)
         qEau = p2k.read_register(2,functioncode=3)
-        qAir = p2k.read_register(4,functioncode=3)
-        pH = p2k.read_register(6,functioncode=3) 
-        pressure = p2k.read_register(8,functioncode=3)
-        qAcid = p2k.read_register(10,functioncode=3)
+        pH = p2k.read_register(3,functioncode=3)
+        pressure = p2k.read_register(4,functioncode=3)
+        qAcid = p2k.read_register(5,functioncode=3)
         cntx+=1
-        
+
+        ## parametres de conversions
+
+        Co2 *= (120000 / 8191)
+        pH /= 10
+
         return Co2,pH,qEau,qAir,pressure,qAcid,cntx,DI
 
 
@@ -231,9 +238,9 @@ def writingToFile(Co2,pH,qEau,qAir,pressure,qAcid,cntx,infich,DI):
     if time.strftime("%H:%M") == '23:55':
         print("it's time to STOP")
         DI = 1
-        
+
     #Bloc de test
-        
+
 ##    if time.strftime("%H:%M") == '11:58':
 ##        print("it's time to STOP")
 ##        DI = 1
@@ -251,12 +258,12 @@ def writingToFile(Co2,pH,qEau,qAir,pressure,qAcid,cntx,infich,DI):
 ##        DI = 1
 ##    elif time.strftime("%H:%M") == '13:56':
 ##        print("it's time to STOP")
-##        DI = 1   
-            
+##        DI = 1
+
 
     else:
         acquisitionTime = 1 # global
-        
+
         #ouverture du fichier CSV en mode ecriture et ecrire dans le fichier
         with open(tempFile,'a') as tmpfile:
             with open(infich, 'a') as csvfile :
@@ -264,30 +271,30 @@ def writingToFile(Co2,pH,qEau,qAir,pressure,qAcid,cntx,infich,DI):
                 #ecrit dans le fichier CSV
                 writer1 = csv.writer(tmpfile)#tmp file
                 writer2 = csv.writer(csvfile)#csv file
-                
+
                 writer1.writerow([cntx, Co2, pH, qEau, qAir,pressure])#tmp file
                 writer2.writerow([time.strftime("%Y-%m-%d"),time.strftime("%H:%M"), 0, 0, qEau, qAcid,
                                   qAir, pH, 0, 0, pressure, 0, 0, Co2, 0])
-        
-        time.sleep(acquisitionTime) 
-        
+
+        time.sleep(acquisitionTime)
+
 
         #Fonction qui va regarder le nombre de ligne du fichier
         #et qui va shifter le contenu du fichier vers le haut.
-        
-        with open(tempFile,'r') as tmpfile:        
+
+        with open(tempFile,'r') as tmpfile:
             checkFile(tmpfile)
 
-    
+
     return DI
 
 
-                
+
 def writingWeeklyReport(cnt,Co2,pH,qEau,qAir,pressure,infich_3):
     cnt += 1
     print(cnt)
     if cnt <= 7 :
-        with open(infich_3, 'a') as csvfile : 
+        with open(infich_3, 'a') as csvfile :
             #ecrit dans le fichier CSV
             writer1 = csv.writer(csvfile)#csv file
             writer1.writerow([time.strftime("%Y-%m-%d"),time.strftime("%H:%M"), 0, 0, qEau, 0,
@@ -305,7 +312,7 @@ def weeklyReport(cnt,infich_2,infich_3):
     a,b,c,d,e,f,g,h,k,l,m,n,o,p,q = [],[],[],[],[],[],[],[],[],[],[],[],[],[],[]
 
     i=0
-    file = open(infich_2, 'r').read() 
+    file = open(infich_2, 'r').read()
 
     lines = file.split('\n')
 
@@ -313,16 +320,16 @@ def weeklyReport(cnt,infich_2,infich_3):
         i+=1
         if len(line) > 1 and i > 1:
             a1,b1,c1,d1,e1,f1,g1,h1,k1,l1,m1,n1,o1,p1,q1 = line.split(',')
-            
+
             e.append(int(e1)) #debit d'eau
             f.append(int(f1)) #debit acide
             g.append(int(g1)) #debit air
             h.append(int(h1)) #pH sensorex
             p.append(int(p1)) #Co2
-            
+
 
     s1,s2,s3,s4,s5 = 0,0,0,0,0
-    
+
     s1 = round(sum(e)/len(e),2)
     s2 = round(sum(f)/len(f),2)
     s3 = round(sum(g)/len(g),2)
@@ -333,7 +340,7 @@ def weeklyReport(cnt,infich_2,infich_3):
 
     return cnt
 
-    
+
 
 
 
@@ -342,26 +349,26 @@ def startLog():
     p2k = minimalmodbus.Instrument('/dev/ttyUSB0',1)
 ##    DI = p2k.read_bit(0)
     cntx,cnt,DI,t =0,7,0,0
-    
+
     while True:
 
         if t != 1 :
-            
- 
+
+
             dataLogFile = 'dataLogging' + time.strftime("%Y-%m-%d") + '.csv'
             infich = './' + dataLogFile
             infich_2 = './Archives/dataLogging' + time.strftime("%Y-%m-%d") + '.csv'
             infich_3 = './Archives/Weekly/dataLogging' + time.strftime("%Y-%m") + '.csv'
             graphName = 'graph' + time.strftime("%Y-%m-%d") + '.png'
-            
-                        
+
+
             with open(infich,'a') as f :
                 writing = csv.writer(f)
                 writing.writerow(headerList)
             t = 1
-                
+
         elif cnt == 7:
-            
+
 
             #ecrire le header du infich, car c'est une autre semaine
             with open(infich_3,'a') as f :
@@ -370,16 +377,16 @@ def startLog():
 
 
             cnt = 0
-            
+
 
         elif DI != 1 :
-            
+
             Co2,pH,qEau,qAir,pressure,qAcid,cntx,DI = sensorReadings(p2k,cntx)
             DI = writingToFile(Co2,pH,qEau,qAir,pressure,qAcid,cntx,infich,DI)
-     
-        
+
+
         else :
-            
+
             print("moving the files")
             #Move le fichier datalog dans le dossier archives
             fileCopy(infich,outdir)
@@ -387,30 +394,13 @@ def startLog():
             plottingDailyReport(graphName,infich_2)
             print("Writting the monthly report")
             cnt = weeklyReport(cnt,infich_2,infich_3)
-            #wait 5 minutes until tomorrow 
+            #wait 5 minutes until tomorrow
             time.sleep(300)
             #time.sleep(60) #For testing
             DI,t = 0,0 #restart the loop
-            
+
 
 
 
 if __name__ == '__main__' :
     startLog()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-
